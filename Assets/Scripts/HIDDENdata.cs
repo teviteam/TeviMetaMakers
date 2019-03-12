cca9d844538afe917c06f624c1f4624528bc9e83
Giovanni <rubinogiov@gmail.com>
2019-03-12 18:59:41 +0000

    Real Initial Commit

---
+++ Assets/Scripts/HIDDENdata.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+
+public static class HIDDENdata {
+    public static bool exists = true;
+    public static string website = "https://metamakers.falmouth.games/";
+    public static string hash = "vlada"; //change and change it in PHP script too
+
+    //http://localhost/
+    //https://www.armaghia.fr/
+    //https://metamakers.falmouth.games/
+}
---
+++ Assets/Scripts/IntroAnim.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAnim : MonoBehaviour {
+
+    public bool skipIntro = false;
+
+    private GameObject connectionObj;
+    private bool myNewPlayer;
+
+    private void Start()
+    {
+        connectionObj = GameObject.Find("ConnectionInfos");
+        if (connectionObj)
+        {
+            myNewPlayer = connectionObj.GetComponent<Connecting>().newPlayer;
+        }
+
+        if (skipIntro||!myNewPlayer) {
+            disableAll();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            disableAll();
+        }
+    }
+
+    public void disableAll  () {
+
+        gameObject.SetActive(false);
+    }
+}
